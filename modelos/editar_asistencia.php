7b179502eeccf08c31d1494cb5cf4a5e11d987c5
Anahi-Gutierrez-22 <gutierrezcardenasanahi@gmail.com>
2025-05-01 18:06:06 -0400

    Update editar_asistencia.php

--- modelos/editar_asistencia.php
+++ modelos/editar_asistencia.php
@@ -1,9 +1,12 @@
 <?php
 include '../modelos/conexion.php';
 
-$asistencias = $conexion->query("SELECT asistencias.id, estudiantes.nombre, estudiantes.ci, estudiantes.ru, asistencias.fecha 
-    FROM asistencias 
-    JOIN estudiantes ON asistencias.estudiante_id = estudiantes.id 
-    ORDER BY asistencias.fecha DESC
-");
+try {
+    $asistencias = $conexion->query("SELECT asistencias.id, estudiantes.nombre, estudiantes.ci, estudiantes.ru, asistencias.fecha FROM asistencias JOIN estudiantes ON asistencias.estudiante_id = estudiantes.id ORDER BY asistencias.fecha DESC ");
+    if (!$asistencias) {
+      throw new Exception("Error al ejecutar la consulta: " . $conexion->error);
+    }
+  } catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+  }
 ?>
