6c59b36ca086bd90f51fe229260a9353dcf1b254
Anahi-Gutierrez-22 <gutierrezcardenasanahi@gmail.com>
2025-05-01 18:05:13 -0400

    Update conexion.php

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -1,7 +1,11 @@
 <?php
-$conexion = new mysqli("localhost", "root", "root", "control_de_asistencias");
-
-if ($conexion->connect_error) {
-    die("Error de conexión: " . $conexion->connect_error);
-}
+try {
+    $conexion = new mysqli("localhost", "root", "root", "control_de_asistencias");
+    if ($conexion->connect_error) {
+      throw new Exception("Error de conexión: " . $conexion->connect_error);
+    }
+  } catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+    exit;
+  }
 ?>
