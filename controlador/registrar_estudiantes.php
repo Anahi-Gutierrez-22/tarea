e1b96041151fada7d2eeb6ec358b5d81005020cc
Anahi-Gutierrez-22 <gutierrezcardenasanahi@gmail.com>
2025-05-01 17:53:06 -0400

    Update registrar_estudiantes.php

--- controlador/registrar_estudiantes.php
+++ controlador/registrar_estudiantes.php
@@ -5,13 +5,16 @@ $nombre = $_POST['nombre'];
 $ci = $_POST['ci'];
 $ru = $_POST['ru'];
 
-$stmt = $conexion->prepare("INSERT INTO estudiantes (nombre, ci, ru) VALUES (?, ?, ?)");
-$stmt->bind_param("sss", $nombre, $ci, $ru);
-
-if ($stmt->execute()) {
-    header("Location: ../vistas/control_de_asistencia.php");
-    exit;
-} else {
-    echo "Error al insertar: " . $stmt->error;
-}
+try {
+    $stmt = $conexion->prepare("INSERT INTO estudiantes (nombre, ci, ru) VALUES (?, ?, ?)");
+    $stmt->bind_param("sss", $nombre, $ci, $ru);
+    if ($stmt->execute()) {
+      header("Location: ../vistas/control_de_asistencia.php");
+      exit;
+    } else {
+      throw new Exception("Error al insertar: " . $stmt->error);
+    }
+  } catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+  }
 ?>
